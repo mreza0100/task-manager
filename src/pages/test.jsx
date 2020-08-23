816512a7ed6b6c2fce104ad479506b47d550ceeb
mreza <mrez9090@gmail.com>
2020-08-23 10:43:17 +0430

    3/6

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,8 +1,17 @@
-import styled from "styled-components";
+// const [selectedDay, setSelectedDay] = useState(null);
 
-export default function ColorPicker() {
-	return <S>getOneFromStategetOneFromS</S>;
-}
-const S = styled.span(props => {
-	return "color: red;";
-});
+// useEffect(() => {
+//       let date = {
+//             year: 1399,
+//             month: 10,
+//             day: 23,
+//       };
+
+//       let str = `${date.year}/${date.month}/${date.day}`;
+
+//       let changedDate = moment(str, "jYYYY/jMM/jDD").locale("en").format("YYYY-MM-DD 00:00:00");
+
+//       console.log(changedDate);
+// }, []);
+
+// return <Calendar value={selectedDay} onChange={setSelectedDay} shouldHighlightWeekends />;
